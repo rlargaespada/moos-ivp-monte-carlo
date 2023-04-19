4a509eb17db914484ea045d73422daf405c5c541
Raul Largaespada <raulalexanderlargaespada@gmail.com>
2023-04-19 11:57:38 -0400

    added initial set of files for performance analyzer

---
+++ src/uEvalPlanner/EvalPlanner.h
@@ -0,0 +1,36 @@
+/************************************************************/
+/*    NAME: Raul Largaespada                                              */
+/*    ORGN: MIT, Cambridge MA                               */
+/*    FILE: EvalPlanner.h                                          */
+/*    DATE: December 29th, 1963                             */
+/************************************************************/
+
+#ifndef EvalPlanner_HEADER
+#define EvalPlanner_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class EvalPlanner : public AppCastingMOOSApp
+{
+ public:
+   EvalPlanner();
+   ~EvalPlanner();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected: // Standard AppCastingMOOSApp function to overload 
+   bool buildReport();
+
+ protected:
+   void registerVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif 
---
+++ src/uEvalPlanner/EvalPlanner_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Raul Largaespada                                             */
+/*   ORGN: MIT, Cambridge MA                                    */
+/*   FILE: EvalPlanner_Info.h                                      */
+/*   DATE: Dececmber 29th, 1963                                 */
+/****************************************************************/
+
+#ifndef EvalPlanner_INFO_HEADER
+#define EvalPlanner_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
