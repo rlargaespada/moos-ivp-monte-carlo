b2628c7c6b4bb526f8957c6da860ba29c11ab730
Raul Largaespada <raulalexanderlargaespada@gmail.com>
2023-05-05 16:06:02 -0400

    added skeleton for LPA*

--- src/pLPAStar/LPAStar.cpp
+++ src/pLPAStar/LPAStar.cpp
@@ -5,18 +5,27 @@
 /*    DATE: December 29th, 1963                             */
 /************************************************************/
 
+#include <iostream>
 #include <iterator>
+#include <string>
 #include "MBUtils.h"
 #include "ACTable.h"
 #include "LPAStar.h"
 
-using namespace std;
 
 //---------------------------------------------------------
 // Constructor()
 
 LPAStar::LPAStar()
 {
+  m_path_found_var = "PATH_FOUND";
+  m_wpt_update_var = "PATH_UPDATE";
+  m_path_complete_var = "PATH_COMPLETE";
+
+  m_grid_density = 2;  // meters
+
+  m_path_requested = false;
+  m_transiting = false;
 }
 
 //---------------------------------------------------------
@@ -34,28 +43,27 @@ bool LPAStar::OnNewMail(MOOSMSG_LIST &NewMail)
   AppCastingMOOSApp::OnNewMail(NewMail);
 
   MOOSMSG_LIST::iterator p;
-  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
+  for (p = NewMail.begin(); p != NewMail.end(); p++) {
     CMOOSMsg &msg = *p;
-    string key    = msg.GetKey();
+    std::string key    = msg.GetKey();
 
-#if 0 // Keep these around just for template
+#if 0  // Keep these around just for template
     string comm  = msg.GetCommunity();
     double dval  = msg.GetDouble();
-    string sval  = msg.GetString(); 
+    string sval  = msg.GetString();
     string msrc  = msg.GetSource();
     double mtime = msg.GetTime();
     bool   mdbl  = msg.IsDouble();
     bool   mstr  = msg.IsString();
 #endif
 
-     if(key == "FOO") 
-       cout << "great!";
+    if (key == "FOO")
+      std::cout << "great!";
 
-     else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
-       reportRunWarning("Unhandled Mail: " + key);
-   }
-	
-   return(true);
+    else if (key != "APPCAST_REQ")  // handled by AppCastingMOOSApp
+      reportRunWarning("Unhandled Mail: " + key);
+  }
+  return(true);
 }
 
 //---------------------------------------------------------
@@ -63,8 +71,8 @@ bool LPAStar::OnNewMail(MOOSMSG_LIST &NewMail)
 
 bool LPAStar::OnConnectToServer()
 {
-   registerVariables();
-   return(true);
+  registerVariables();
+  return (true);
 }
 
 //---------------------------------------------------------
@@ -76,7 +84,7 @@ bool LPAStar::Iterate()
   AppCastingMOOSApp::Iterate();
   // Do your thing here!
   AppCastingMOOSApp::PostReport();
-  return(true);
+  return (true);
 }
 
 //---------------------------------------------------------
@@ -89,30 +97,46 @@ bool LPAStar::OnStartUp()
 
   STRING_LIST sParams;
   m_MissionReader.EnableVerbatimQuoting(false);
-  if(!m_MissionReader.GetConfiguration(GetAppName(), sParams))
+  if (!m_MissionReader.GetConfiguration(GetAppName(), sParams))
     reportConfigWarning("No config block found for " + GetAppName());
 
   STRING_LIST::iterator p;
-  for(p=sParams.begin(); p!=sParams.end(); p++) {
-    string orig  = *p;
-    string line  = *p;
-    string param = tolower(biteStringX(line, '='));
-    string value = line;
+  for (p = sParams.begin(); p != sParams.end(); p++) {
+    std::string orig  = *p;
+    std::string line  = *p;
+    std::string param = tolower(biteStringX(line, '='));
+    std::string value = line;
 
     bool handled = false;
-    if(param == "foo") {
-      handled = true;
-    }
-    else if(param == "bar") {
-      handled = true;
+    if (param == "path_request_var") {
+      handled = setNonWhiteVarOnString(m_path_request_var, value);
+    } else if (param == "obs_alert_var") {
+      handled = setNonWhiteVarOnString(m_obs_alert_var, value);
+    } else if (param == "path_found_var") {
+      handled = setNonWhiteVarOnString(m_path_found_var, value);
+    } else if (param == "wpt_update_var") {
+      handled = setNonWhiteVarOnString(m_wpt_update_var, value);
+    } else if (param == "wpt_complete_var") {
+      handled = setNonWhiteVarOnString(m_wpt_complete_var, value);
+    } else if (param == "path_complete_var") {
+      handled = setNonWhiteVarOnString(m_path_complete_var, value);
     }
 
-    if(!handled)
+    if (!handled)
       reportUnhandledConfigWarning(orig);
-
   }
-  
-  registerVariables();	
+
+  // fill in subscription variables with defaults if they weren't set in config
+  // leave these variables empty in constructor so that we don't register for
+  // variables we don't need
+  if (m_path_request_var.empty())
+    m_path_request_var = "PLAN_PATH_REQUESTED";
+  if (m_obs_alert_var.empty())
+    m_obs_alert_var = "OBSTACLE_ALERT";
+  if (m_wpt_complete_var.empty())
+    m_wpt_complete_var = "WAYPOINTS_COMPLETE";
+
+  registerVariables();
   return(true);
 }
 
@@ -129,18 +153,9 @@ void LPAStar::registerVariables()
 //------------------------------------------------------------
 // Procedure: buildReport()
 
-bool LPAStar::buildReport() 
+bool LPAStar::buildReport()
 {
-  m_msgs << "============================================" << endl;
-  m_msgs << "File:                                       " << endl;
-  m_msgs << "============================================" << endl;
-
-  ACTable actab(4);
-  actab << "Alpha | Bravo | Charlie | Delta";
-  actab.addHeaderLines();
-  actab << "one" << "two" << "three" << "four";
-  m_msgs << actab.getFormattedString();
-
+  using std::endl;
   return(true);
 }
 
--- src/pLPAStar/LPAStar.h
+++ src/pLPAStar/LPAStar.h
@@ -8,29 +8,69 @@
 #ifndef LPAStar_HEADER
 #define LPAStar_HEADER
 
+#include <map>
+#include <string>
+#include <vector>
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include <XYPoint.h>
+#include <XYPolygon.h>
 
 class LPAStar : public AppCastingMOOSApp
 {
  public:
-   LPAStar();
-   ~LPAStar();
+  LPAStar();
+  ~LPAStar();
 
- protected: // Standard MOOSApp functions to overload  
-   bool OnNewMail(MOOSMSG_LIST &NewMail);
-   bool Iterate();
-   bool OnConnectToServer();
-   bool OnStartUp();
+ protected:  // Standard MOOSApp functions to overload
+  bool OnNewMail(MOOSMSG_LIST &NewMail);
+  bool Iterate();
+  bool OnConnectToServer();
+  bool OnStartUp();
 
- protected: // Standard AppCastingMOOSApp function to overload 
-   bool buildReport();
+ protected:  // Standard AppCastingMOOSApp function to overload
+  bool buildReport();
 
  protected:
-   void registerVariables();
+  void registerVariables();
 
- private: // Configuration variables
+  // mail handling
+  bool setEndpoints(std::string request);
 
- private: // State variables
+  // path planning
+  bool clearGrid();
+  bool addObsToGrid();
+  bool planPath();
+
+  // path publishing
+  double getPathLength();
+  std::string getPathSpec();
+  bool postPath();
+
+  // replanning
+  bool checkObstacle();
+
+ private:  // Configuration variables
+  std::string m_path_request_var;  // PLAN_PATH_REQUESTED (s)
+
+  std::string m_obs_alert_var;  // OBSTACLE_ALERT (s)
+
+  std::string m_path_found_var;  // PATH_FOUND (p)
+  std::string m_wpt_update_var;  // PATH_UPDATE (p)
+  std::string m_wpt_complete_var;  // WAYPOINTS_COMPLETE (s)
+  std::string m_path_complete_var;  // PATH_COMPLETE (p)
+
+  double m_grid_density;  // todo: think about how this would work
+ private:  // State variables
+  XYPoint m_start_point;
+  XYPoint m_goal_point;
+
+  std::map<std::string, XYPolygon> m_given_obstacles;
+  std::map<std::string, XYPolygon> m_alerted_obstacles;  // todo: how long do obs stay in here?
+
+  bool m_path_requested;
+  bool m_transiting;
+
+  std::vector<XYPoint> m_path;
 };
 
-#endif 
+#endif
