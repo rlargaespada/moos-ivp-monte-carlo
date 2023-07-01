06d65e14e40ac2fde3aeb40d69422a3844f6f2a5
Raul Largaespada <raulalexanderlargaespada@gmail.com>
2023-06-30 23:45:16 -0400

    formatted and commented IRISProblem.run()

--- src/pIRIS2D/IRISProblem.cpp
+++ src/pIRIS2D/IRISProblem.cpp
@@ -55,28 +55,32 @@ bool IRISProblem::run(int num_iters)
   int iters{0};
 
   while (iters < num_iters) {
+    // run iris iteration
     IRISPolygon new_poly{separating_lines(m_obstacles, m_ellipse)};
     new_poly.appendConstraints(m_bounds);
     m_polygon = new_poly;
-
     area = inner_ellipsoid(new_poly, &m_ellipse);
 
+    // increment counters
     iters++;
     m_iters_done++;
-    if ((std::abs(area - m_best_area)/ m_best_area) < m_termination_threshold) {
+
+    // check termination conditions
+    if (((std::abs(area - m_best_area)/ m_best_area) < m_termination_threshold) ||
+        (m_iters_done >= m_max_iters)) {
       m_complete = true;
-      return (true);  // problem is done
-    } else if (m_iters_done >= m_max_iters) {
-      m_complete = true;
-      return (true);  // problem is done
-    } else if (iters >= num_iters) {
       break;
     }
 
+    // if we exceed num_iters without hitting other termination conditions,
+    // IRIS hasn't converged yet and we need to continue later
     m_best_area = area;
+    if (iters >= num_iters) {
+      break;
+    }
   }
 
-  return (false);  // still work to go
+  return (m_complete);
 }
 
 
@@ -85,6 +89,7 @@ bool IRISProblem::run(int num_iters)
 
 void IRISProblem::addObstacle(XYPolygon obstacle)
 {
+  // pull out vertices of obstacle and place into a (2 x num_vertices) size matrix
   Eigen::Matrix2Xd obs_matrix(2, obstacle.size());
   for (int i{0}; i < obstacle.size(); i++)
     obs_matrix.col(i) << obstacle.get_vx(i), obstacle.get_vy(i);
